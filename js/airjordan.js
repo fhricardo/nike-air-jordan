7d94b53cc648ec4e4202d4144f5ad3e7580dc835
Flavio Ricardo <fhricardo@gmail.com>
2023-07-13 15:53:34 -0300

    Script File Update

--- js/airjordan.js
+++ js/airjordan.js
@@ -2,28 +2,19 @@ document.addEventListener("contextmenu", (event) => {
     event.preventDefault();
 });
 var thisColor = "red"
-console.log(thisColor)
 function changeImage(img) {
-
-    console.log(img)
     let picture = document.getElementById("large")
     picture.src = "assets/" + thisColor + "-" + img + ".jpg"
-    console.log(thisColor)
 }
 function colorSelector(color) {
     thisColor = color
-    console.log(thisColor)
-
     var theImage = 1;
     while (theImage <= 8) {
         let cod = `aj${theImage}`
-        console.log(cod)
-        console.log(theImage)
         thumbFile = `assets/${thisColor}-${theImage}.jpg`
         var thumb = document.getElementById(cod)
         thumb.src = thumbFile
         theImage++
     }
     changeImage(1)
-
 }
